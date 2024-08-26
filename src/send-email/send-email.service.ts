2ca7dae56ed206e1c37ae8d4c94f497eeff1ed8b
ElNao28 <naopubg28@gmail.com>
2024-08-26 14:26:08 -0600

    estilos a los correos

--- src/send-email/send-email.service.ts
+++ src/send-email/send-email.service.ts
@@ -17,14 +17,14 @@ export class SendEmailService {
     const info = await transporter.sendMail({
       from: '"Contacto Silsa Consultores" <silsaconsultores88@gmail.com>',
       to: "luis28jair5@gmail.com",
-      subject: "Se a recibido un email del formulario contacto",
+      subject: "Mensaje desde el formulario contacto",
       html: `<div style="padding: 10px; font-size: large;">
-      <h2>El contenido es el siguiente:</h2>
-      <p><strong>Nombre:</strong> ${data.name}</p>
-      <p><strong>Email:</strong> ${data.email}</p>
-      <p><strong>Telefono:</strong> ${data.cellphone}</p>
-      <p><strong>Asunto: <br></strong> <i>${data.message}</i></p>
-  </div>`
+           <h2>Informacion de contacto:</h2>
+           <p><strong>Nombre:</strong> ${data.name}</p>
+           <p><strong>Email:</strong> ${data.email}</p>
+           <p><strong>Telefono:</strong> ${data.cellphone}</p>
+           <p><strong>Asunto: <br></strong> <i>${data.message}</i></p>
+       </div>`
     });
     console.log("Message sent: %s", info.messageId);
     return {
@@ -42,24 +42,35 @@ export class SendEmailService {
       from: '"Contacto Silsa Consultores" <silsaconsultores88@gmail.com>',
       to: to,
       subject: "Confirmacion de cita",
-      html: `<section style="display: flex; justify-content: center;">
-        <div
-            style="font-size: 20px; width: 80%; border-radius: 5px;background-color: rgb(245, 245, 245); padding-bottom: 10px; padding: 20px;">
-            <h1
-                style="background-color: rgb(187, 187, 187);border-radius: 5px; text-align: center; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
-                Confirmacion de Cita
-            </h1>
-            <div style="font-size: 20px;">
-                <p>Para confirmar la solicitud de su cita, porfavor abra el siguiente enlace:</p>
-                <a href="http://localhost:4200/confirmar-cita/${code}"
-                    style="text-decoration: none; color: rgb(2, 2, 207);">Click aqui para confirmar su
-                    cita</a>
-                <p>Si no haz realizado algun tramite de cita y desconozcas este movimiento, puedes hacer caso omiso a
-                    este mensaje o ponerte en contacto con nostros al contacto:</p> <span
-                    style="color: rgb(2, 2, 207);">silsaconsultores88@gmail.com</span>
-            </div>
-        </div> 
-    </section>`
+      html: `<body>
+    <div style="display: flex; justify-content: center;">
+        <h1
+            style="text-align: center; font-family:Arial, Helvetica, sans-serif; color:rgb(255, 255, 255);background-color: rgb(76, 111, 252); padding: 10px; border-radius: 2px;width: 60%;">
+            Confimacion de sita
+        </h1>
+    </div>
+    <div">
+        <strong style="display: flex; justify-content: center; margin-bottom: 20px;">
+            <p
+                style="text-align: justify; font-size: large; font-family: Arial, Helvetica, sans-serif; color: rgb(134, 130, 130); width: 60%;">
+                Has solicitado una sita, si deseas continuar con el proceso de la sita porfavor confirma esta pulsando
+                el siguiente boton, una vez lo pulses tu sita quedara agendada.
+            </p>
+        </strong>
+        <div style="display: flex; justify-content: center; margin-bottom: 20px;">
+            <strong><a href="http://localhost:4200/confirmar-cita/${code}"
+                    style="font-size: large; text-align: center; text-decoration: none; background-color: rgb(41, 143, 232); color: white; padding: 10px; border-radius: 3px;font-family: Arial, Helvetica, sans-serif;">Confirmar
+                    sita</a></strong>
+        </div>
+        <i style="display: flex; justify-content: center;">
+            <p
+                style="font-size: large; font-family: Arial, Helvetica, sans-serif; color: rgb(134, 130, 130); width: 60%;">
+                Si no has realizado algun tramite de cita y desconozces este movimiento, puedes hacer caso omiso a este
+                mensaje o ponerte en contacto con nostros al contacto: <span>silsaconsultores88@gmail.com</span></p>
+        </i>
+
+        </div>
+</body>`
     });
     console.log("Message sent: %s", info.messageId);
     return code;
